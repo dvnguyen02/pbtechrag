4e6132aa10fdaf3efa556fb81f6e8beb770ad34c
dvnguyen02 <duynguyen290502@gmail.com>
2025-04-17 17:15:09 +1200

    fix: ready to deploy again

--- backend/server.py
+++ backend/server.py
@@ -131,9 +131,9 @@ def reset_conversation():
     
     return jsonify({"success": True, "new_thread_id": new_thread_id})
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
 # if __name__ == '__main__':
-#     port = int(os.environ.get('PORT', 10000))
-#     app.run(host='0.0.0.0', port=port)
+#     app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
